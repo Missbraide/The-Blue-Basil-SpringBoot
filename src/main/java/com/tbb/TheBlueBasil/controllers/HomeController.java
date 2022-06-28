d0ecfa11ae9d113d4a88d15c9f7a1f28df0ea523
Ella <e.braide@yahoo.com>
2022-06-28 14:35:18 -0700

    added controllers for views and replaced headers and footers codes with fragrams.

--- src/main/java/com/tbb/TheBlueBasil/controllers/HomeController.java
+++ src/main/java/com/tbb/TheBlueBasil/controllers/HomeController.java
@@ -1,8 +1,21 @@
 package com.tbb.TheBlueBasil.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
     @GetMapping(value = {"/", "index"})
     public String viewHomePage(){return "index";}
+
+    @GetMapping(value = {"/our-story"})
+    public String viewOurStoryPage(){return "our-story";}
+
+    @GetMapping(value = {"/menu"})
+    public String viewOurMenuPage(){return "menu";}
+
+
+    @GetMapping(value = {"/reservation"})
+    public String viewReservationPage(){return "reservation";}
+
 }
