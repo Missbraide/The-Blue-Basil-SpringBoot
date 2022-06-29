472dd0957b5e8af0caee098a6a81b661d16587ca
Ella <e.braide@yahoo.com>
2022-06-28 19:46:44 -0700

    added tests methods

---
+++ src/test/java/com/tbb/TheBlueBasil/models/ReservationTest.java
@@ -0,0 +1,96 @@
+package com.tbb.TheBlueBasil.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getResFirstName() {
+    }
+
+    @Test
+    void getResLastName() {
+    }
+
+    @Test
+    void getResDate() {
+    }
+
+    @Test
+    void getResPhone() {
+    }
+
+    @Test
+    void getResEmail() {
+    }
+
+    @Test
+    void getResNoOfGuests() {
+    }
+
+    @Test
+    void getAddInfo() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setResFirstName() {
+    }
+
+    @Test
+    void setResLastName() {
+    }
+
+    @Test
+    void setResDate() {
+    }
+
+    @Test
+    void setResPhone() {
+    }
+
+    @Test
+    void setResEmail() {
+    }
+
+    @Test
+    void setResNoOfGuests() {
+    }
+
+    @Test
+    void setAddInfo() {
+    }
+
+    @Test
+    void setUsers() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/test/java/com/tbb/TheBlueBasil/models/UserTest.java
@@ -0,0 +1,97 @@
+package com.tbb.TheBlueBasil.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void getId() {
+
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void isManager() {
+    }
+
+    @Test
+    void isCustomer() {
+    }
+
+    @Test
+    void isEmployee() {
+    }
+
+    @Test
+    void getReservation() {
+    }
+
+    @Test
+    void getMenu() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void setManager() {
+    }
+
+    @Test
+    void setCustomer() {
+    }
+
+    @Test
+    void setEmployee() {
+    }
+
+    @Test
+    void setReservation() {
+    }
+
+    @Test
+    void setMenu() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
