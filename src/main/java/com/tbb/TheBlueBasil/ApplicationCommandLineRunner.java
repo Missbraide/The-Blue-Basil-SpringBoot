5d1bdb0c24c2bc46e6d6e94570150207ea61ef4b
Ella <e.braide@yahoo.com>
2022-06-28 19:45:29 -0700

    updated command line runner

--- src/main/java/com/tbb/TheBlueBasil/ApplicationCommandLineRunner.java
+++ src/main/java/com/tbb/TheBlueBasil/ApplicationCommandLineRunner.java
@@ -1,20 +1,54 @@
 package com.tbb.TheBlueBasil;
 
+import com.tbb.TheBlueBasil.data.AuthGroupRepository;
+import com.tbb.TheBlueBasil.data.MenuRepository;
+import com.tbb.TheBlueBasil.data.UserRepository;
+import com.tbb.TheBlueBasil.models.AuthGroup;
+import com.tbb.TheBlueBasil.models.Reservation;
+import com.tbb.TheBlueBasil.models.User;
+import com.tbb.TheBlueBasil.services.ReservationService;
+import com.tbb.TheBlueBasil.services.UserService;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component @Slf4j
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class ApplicationCommandLineRunner  implements CommandLineRunner {
-//    ReservationService reservationService;
-//    static final String PASSWORD
-//
+    ReservationService reservationService;
+    AuthGroupRepository authGroupRepository;
+    UserRepository   userRepository;
+    MenuRepository menuRepository;
+
+    public ApplicationCommandLineRunner(ReservationService reservationService, AuthGroupRepository authGroupRepository, UserRepository userRepository, MenuRepository menuRepository) {
+        this.reservationService = reservationService;
+        this.authGroupRepository = authGroupRepository;
+        this.userRepository = userRepository;
+        this.menuRepository = menuRepository;
+    }
+
+    static final  String PASSWORD = "password";
+    static final  String ELLAID = "ella@gmail.com";
+    static final  String BROOKEID ="brooke@gmail.com";
+    static final  String JAXID = "jax@gmail.com";
+    static final  String ROLE_ADMIN = "ROLE_ADMIN";
+    static final  String ROLE_USER = "ROLE_USER";
+
+    @PostConstruct
+    public void postConstruct(){
+        log.warn("============ Application CommandLine Runner ============");
+    }
 
     @Override
     public void run(String... args) throws Exception {
+
+
+
+
 
     }
 }
