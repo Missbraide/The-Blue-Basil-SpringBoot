a5c88d8482bdb6515b0a6674b5d6efb5d83949b2
Ella <e.braide@yahoo.com>
2022-06-30 15:54:50 -0700

    created unit test

--- src/test/java/com/tbb/TheBlueBasil/models/ReservationTest.java
+++ src/test/java/com/tbb/TheBlueBasil/models/ReservationTest.java
@@ -6,91 +6,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationTest {
 
-    @Test
-    void testEquals() {
-    }
 
     @Test
-    void canEqual() {
+    void getResName() {
+        Reservation reservation = new Reservation("Ella","Braide", "1234567890", "ella@gmail.com", "4", "Table by the window" );
+        assertEquals("Ella", reservation.getResFirstName());
     }
 
-    @Test
-    void testHashCode() {
-    }
 
-    @Test
-    void getId() {
-    }
-
-    @Test
-    void getResFirstName() {
-    }
-
-    @Test
-    void getResLastName() {
-    }
-
-    @Test
-    void getResDate() {
-    }
-
-    @Test
-    void getResPhone() {
-    }
-
-    @Test
-    void getResEmail() {
-    }
-
-    @Test
-    void getResNoOfGuests() {
-    }
-
-    @Test
-    void getAddInfo() {
-    }
-
-    @Test
-    void getUsers() {
-    }
-
-    @Test
-    void setId() {
-    }
-
-    @Test
-    void setResFirstName() {
-    }
-
-    @Test
-    void setResLastName() {
-    }
-
-    @Test
-    void setResDate() {
-    }
-
-    @Test
-    void setResPhone() {
-    }
-
-    @Test
-    void setResEmail() {
-    }
-
-    @Test
-    void setResNoOfGuests() {
-    }
-
-    @Test
-    void setAddInfo() {
-    }
-
-    @Test
-    void setUsers() {
-    }
-
-    @Test
-    void testToString() {
-    }
 }
