75f5f7b8dd92331af29a8c1cbb04d559622c6707
Ella <e.braide@yahoo.com>
2022-06-29 20:12:09 -0700

    updated

--- src/main/java/com/tbb/TheBlueBasil/controllers/HomeController.java
+++ src/main/java/com/tbb/TheBlueBasil/controllers/HomeController.java
@@ -8,34 +8,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping
-    String getIndex(){
+    String getIndex() {
         return "index";
     }
+
     @GetMapping(value = {"/", "index"})
-    public String viewHomePage(){return "index";}
+    public String viewHomePage() {
+        return "index";
+    }
 
     @GetMapping(value = {"/our-story"})
-    public String viewOurStoryPage(){return "our-story";}
+    public String viewOurStoryPage() {
+        return "our-story";
+    }
 
     @GetMapping(value = {"/menu"})
-    public String viewOurMenuPage(){return "menu";}
+    public String viewOurMenuPage() {
+        return "menu";
+    }
 
 
     @GetMapping(value = {"/reservation"})
-    public String viewReservationPage(){return "reservation";}
+    public String viewReservationPage() {
+        return "reservation";
+    }
 
 
     @GetMapping(value = {"/gallery"})
-    public String viewGalleryPage(){return "gallery";}
+    public String viewGalleryPage() {
+        return "gallery";
+    }
 
     @GetMapping(value = {"/get-in-touch"})
-    public String viewGetInTouchPage(){return "get-in-touch";}
+    public String viewGetInTouchPage() {
+        return "get-in-touch";
+    }
 
     @GetMapping(value = {"/login"})
-    public String viewLoginPage(){return "login";}
+    public String viewLoginPage() {
+        return "login";
+    }
 
-}
 
+//    @GetMapping(value = {"/signup"})
+//    public String viewSignUpPage(){return "signup";}
+//
+//}
 
 
 //    @GetMapping("/login")
@@ -49,3 +67,4 @@ public class HomeController {
 //    }
 //}
 
+}
