fd38672452623dd1470e26ec836f4a30cba4156b
Ella <e.braide@yahoo.com>
2022-07-01 14:28:47 -0700

    updated constructor

--- src/test/java/com/tbb/TheBlueBasil/models/ReservationTest.java
+++ src/test/java/com/tbb/TheBlueBasil/models/ReservationTest.java
@@ -2,6 +2,9 @@ package com.tbb.TheBlueBasil.models;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationTest {
@@ -9,7 +12,7 @@ class ReservationTest {
 
     @Test
     void getResName() {
-        Reservation reservation = new Reservation("Ella","Braide", "1234567890", "ella@gmail.com", "4", "Table by the window" );
+        Reservation reservation = new Reservation("Ella","Braide", LocalDate.now(),  LocalTime.of(12,00,00), "1234567890", "ella@gmail.com", "4", "Table by the window");
         assertEquals("Ella", reservation.getResFirstName());
     }
 
