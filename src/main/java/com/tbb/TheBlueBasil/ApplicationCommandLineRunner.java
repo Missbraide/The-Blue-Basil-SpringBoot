f971a24b540287b71531f06d6b8035932d2adbcf
Ella <e.braide@yahoo.com>
2022-06-30 22:10:50 -0700

    added menu and items

--- src/main/java/com/tbb/TheBlueBasil/ApplicationCommandLineRunner.java
+++ src/main/java/com/tbb/TheBlueBasil/ApplicationCommandLineRunner.java
@@ -1,11 +1,11 @@
 package com.tbb.TheBlueBasil;
 
-import com.tbb.TheBlueBasil.data.AuthGroupRepository;
-import com.tbb.TheBlueBasil.data.MenuRepository;
-import com.tbb.TheBlueBasil.data.ReservationRepository;
-import com.tbb.TheBlueBasil.data.UserRepository;
+import com.tbb.TheBlueBasil.data.*;
+import com.tbb.TheBlueBasil.models.Item;
+import com.tbb.TheBlueBasil.models.Menu;
 import com.tbb.TheBlueBasil.models.Reservation;
 import com.tbb.TheBlueBasil.models.User;
+import com.tbb.TheBlueBasil.services.MenuService;
 import com.tbb.TheBlueBasil.services.ReservationService;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
@@ -29,15 +29,19 @@ public class ApplicationCommandLineRunner  implements CommandLineRunner {
     AuthGroupRepository authGroupRepository;
     UserRepository   userRepository;
     MenuRepository menuRepository;
+    MenuService menuService;
+    ItemRepository itemRepository;
 
-    public ApplicationCommandLineRunner( ReservationRepository reservationRepository, AuthGroupRepository authGroupRepository, UserRepository userRepository, MenuRepository menuRepository) {
+    public ApplicationCommandLineRunner(ReservationRepository reservationRepository, AuthGroupRepository authGroupRepository, UserRepository userRepository, MenuRepository menuRepository, MenuService menuService, ItemRepository itemRepository) {
         this.reservationRepository = reservationRepository;
         this.authGroupRepository = authGroupRepository;
         this.userRepository = userRepository;
         this.menuRepository = menuRepository;
+        this.menuService = menuService;
+        this.itemRepository = itemRepository;
     }
 
-//    static final  String PASSWORD = "password";
+    //    static final  String PASSWORD = "password";
 //    static final  String ELLAID = "ella@gmail.com";
 //    static final  String BROOKEID ="brooke@gmail.com";
 //    static final  String JAXID = "jax@gmail.com";
@@ -62,10 +66,19 @@ public class ApplicationCommandLineRunner  implements CommandLineRunner {
         userRepository.save(new User("Joy", "Rex", "joy@gmail.com", "password"));
 
 
-        reservationRepository.save(new Reservation("Ella","Braide", LocalDate.now(),  LocalTime.of(12,00,00), "1234567890", "ella@gmail.com", "4", "Table by the window"));
+        reservationRepository.save(new Reservation(1L,"Ella","Braide", LocalDate.now(),  LocalTime.of(12,00,00), "1234567890", "ella@gmail.com", "4", "Table by the window"));
         reservationRepository.save(new Reservation("Lola","Oredola", LocalDate.now(), LocalTime.of(15,00,00), "1234567890", "lola@gmail.com", "2", "Need high chair for baby"));
         reservationRepository.save(new Reservation("Arit","Jack", LocalDate.now(), LocalTime.of(11,30,00),"1234567890", "arit@gmail.com", "6", "Table by the window"));
         reservationRepository.save(new Reservation("Jax","Teller", LocalDate.now(),LocalTime.of(20,00,00), "1234567890", "jax@gmail.com", "2", "By the bar"));
         reservationRepository.save(new Reservation("Joy","Rex", LocalDate.now(),LocalTime.of(14,15,00), "1234567890", "joy@gmail.com", "2", "Candles"));
+        reservationRepository.save(new Reservation("Mary","Joe", LocalDate.now(), LocalTime.of(12,00,00), "123457890", "mary@gmail.com", "4", "table"));
+
+
+
+        menuRepository.save(new Menu("breakfast", "bread"));
+
+        itemRepository.save(new Item("Avocado salad", "avocado", 6.99, true));
+//        menuService.addItemToMenu(1,1);
+
     }
 }
