104ea361ec139d8c96b2b28ea8d7bb2a54f92bcc
Ella <e.braide@yahoo.com>
2022-07-01 19:56:16 -0700

    updated

--- src/main/java/com/tbb/TheBlueBasil/controllers/ReservationController.java
+++ src/main/java/com/tbb/TheBlueBasil/controllers/ReservationController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Slf4j
 @Controller
@@ -51,11 +51,9 @@ public class ReservationController {
     }
     @PostMapping("/saveReservation")
     public String saveReservation(@ModelAttribute("reservation") Reservation reservation) {
-       log.info("New Reservation");
-       log.info("Reservation before save {}", reservation);
-       if (userRepository.findById(reservation.getResEmail()).isEmpty() )
+              if (userRepository.findById(reservation.getId()).isEmpty() )
            return "redirect:/showNewReservationForm";
-       reservation.setUser(userRepository.findById(reservation.getResEmail()).get());
+       reservation.setUser(userRepository.findById(reservation.getId()).get());
         // save reservation to database
        reservationService.saveReservation(reservation);
         return "redirect:/listReservations";
