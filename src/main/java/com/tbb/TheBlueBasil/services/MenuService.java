2c9ba7b30198f832c7b52cefa56b3ccb5b03b5e6
Ella <e.braide@yahoo.com>
2022-07-01 23:24:35 -0700

    updated menu service

--- src/main/java/com/tbb/TheBlueBasil/services/MenuService.java
+++ src/main/java/com/tbb/TheBlueBasil/services/MenuService.java
@@ -1,13 +1,29 @@
 package com.tbb.TheBlueBasil.services;
 
+import com.tbb.TheBlueBasil.data.ItemRepository;
+import com.tbb.TheBlueBasil.data.MenuRepository;
+import com.tbb.TheBlueBasil.models.Item;
 import com.tbb.TheBlueBasil.models.Menu;
 import com.tbb.TheBlueBasil.models.Reservation;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 @Service
 public class MenuService {
+
+    MenuRepository menuRepository;
+    ItemRepository itemRepository;
+
+    public MenuService(MenuRepository menuRepository, ItemRepository itemRepository) {
+        this.menuRepository = menuRepository;
+        this.itemRepository = itemRepository;
+    }
 
     public List<Menu> getAllMenus() {
         return null;
@@ -26,6 +42,28 @@ public class MenuService {
 
 
     public void deleteMenuById(long id) {
+
+    }
+
+
+//    @Transactional(rollbackOn = {NoSuchElementException.class})
+//    public void addCourse(String email, Course c) throws NoSuchElementException{
+//
+//        Student s = studentRepository.findById(email).orElseThrow();
+//        c = courseRepository.save(c);
+//        s.addCourse(c);
+//        studentRepository.save(s);
+//    }
+    // action allowed by admin only
+
+    @PersistenceContext
+    EntityManager em;
+    @Transactional(rollbackOn = {NoSuchElementException.class})
+    public void addItemToMenu(long id, long item_id){
+        Menu menu = em.find(Menu.class, id);
+        Item item = em.find(Item.class, item_id);
+        menu.getItems().add(item);
+        item.setMenus((Set<Menu>) menu);
 
     }
 }
